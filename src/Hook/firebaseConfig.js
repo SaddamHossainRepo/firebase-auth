f9bd12bf36c51e988f9c816cd0f5806b4f90d6ab
Saddam <saddam13@cse.pstu.ac.bd>
2023-01-31 14:10:46 +0600

    first commit

---
+++ src/Hook/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb4JWcsMZORaftm6M8fH8xNKtthTif_8A",
+  authDomain: "fire-auth-b176d.firebaseapp.com",
+  projectId: "fire-auth-b176d",
+  storageBucket: "fire-auth-b176d.appspot.com",
+  messagingSenderId: "524564751166",
+  appId: "1:524564751166:web:490a3fcad93152bd8ac26f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,181 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
+import app from './../../Hook/firebaseConfig';
+import Swal from 'sweetalert2';
+import useFirebase from "../../Hook/useFirebase";
+
+
+const Registration = ({user, setUser}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+  const auth = getAuth(app);
+  const {handleGoogleLogin, test2} = useFirebase();
+
+
+  const handleName = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
+
+  const handleEmail = (e) => {
+    // const test = /\S+@\S+\.S+/.test(e.target.value);
+    const test = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e.target.value);
+    if (!test) {
+      setError('Please give a valid email');
+      return;
+    }
+    else {
+      setError('')
+    }
+    // console.log(test);
+    setEmail(e.target.value);
+  }
+  console.log('name: ', name);
+  // console.log(email);
+
+  const handlePassword = (e) => {
+    if (!/(?=.{8,})/.test(e.target.value)) {
+      setError('Password must be 8 characters')
+      return;
+    }
+    if (!/(?=.*[a-zA-Z])/.test(e.target.value)) {
+      setError('Please add at least one uppercase')
+      return;
+    }
+    if (!/(?=.*[!#@$%&? "])/.test(e.target.value)) {
+      setError('Please add at least one special character')
+      return;
+    }
+    setError('')
+    setPassword(e.target.value);
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    if (name, email, password) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const userInfo = userCredential.user;
+          setUser(userInfo);
+          updateName();
+          verify();
+          Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+          )
+          // console.log(userInfo);
+          setError('');
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage)
+          // ..
+        });
+    }
+    else {
+      setError('Please fill out all the input')
+    }
+  }
+
+  const updateName = () => {
+    updateProfile(auth.currentUser, {
+      displayName: "Jane Q. User", photoURL: "https://example.com/jane-q-user/profile.jpg"
+    }).then(() => {
+      // Profile updated!
+      // ...
+    }).catch((error) => {
+      // An error occurred
+      // ...
+    });
+  }
+
+  const verify = () => {
+    const auth = getAuth();
+    sendEmailVerification(auth.currentUser)
+      .then(() => {
+        // Email verification sent!
+        // ...
+      });
+  }
+  return (
+    <div className="mt-5">
+      <div className="main-container d-flex container justify-content-between align-items-center">
+        <div className="register-image image-fluid w-100  ">
+          <img
+            className="w-100 img-fluid image-fluid"
+            src="https://i.ibb.co/hYJTmVX/undraw-Mobile-login-re-9ntv-1.png"
+            alt=""
+          />
+        </div>
+        <div className="register-form  w-100">
+          <div className="input-box">
+            <p className="text-danger">{error}</p>
+            <form action="">
+              <input
+                onBlur={handleName}
+                className="form-control p-3 m-2"
+                type="text"
+                placeholder="Enter your name"
+                required
+              />
+              <input
+                onBlur={handleEmail}
+                className="form-control p-3 m-2"
+                type="email"
+                placeholder="Email"
+                required
+              />
+              <input
+                onBlur={handlePassword}
+                className="form-control p-3 m-2"
+                type="password"
+                placeholder="password"
+                required
+              />
+              <p className="link ">
+                <Link to="/login" className="text-decoration-none">
+                  <small className="text-danger link">
+                    already have an account? please login
+                  </small>
+                </Link>
+              </p>
+              <input 
+              onClick={()=> setIsDisabled(!isDisabled)}
+              className="p-2" type="checkbox" />{" "}
+              <span className="mb-3">accept term & condition</span>
+              <br />
+              <button
+                onClick={handleRegister}
+                type="submit"
+                className="btn btn-info p-3 w-50 mt-3 fw-bold text-white"
+                disabled={isDisabled}
+              >
+                Register
+              </button>
+            </form>
+          </div>
+          <button 
+          onClick={handleGoogleLogin}
+          className="btn mt-3 border d-flex align-items-center justify-content-evenly p-2 m-auto">
+            <img
+              className="w-25 image-fluid btn-image"
+              src="https://img.icons8.com/color/344/google-logo.png"
+              alt=""
+            />
+            <p className="fw-bold">Google SignIn</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
